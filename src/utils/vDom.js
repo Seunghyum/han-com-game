e4a64e310d662829e59f65b1f550a8ea6e4323dd
seunghyum <antmgu@gmail.com>
2021-02-08 14:45:18 +0900

    close #8 - feat: add vDom for utils
    add vDom for append once in <div id="app"></div>
    it will be used in components and pages for dom

---
+++ src/utils/vDom.js
@@ -0,0 +1,42 @@
+function appendText(el, text) {
+  const textNode = document.createTextNode(text);
+  el.appendChild(textNode);
+}
+
+function appendArray(el, children) {
+  children.forEach((child) => {
+    if (Array.isArray(child)) {
+      appendArray(el, child);
+    } else if (child instanceof window.Element) {
+      el.appendChild(child);
+    } else if (typeof child === `string`) {
+      appendText(el, child);
+    }
+  });
+}
+
+function makeElement(type, textOrPropsOrChild, ...otherChildren) {
+  const el = document.createElement(type);
+
+  if (Array.isArray(textOrPropsOrChild)) {
+    appendArray(el, textOrPropsOrChild);
+  } else if (textOrPropsOrChild instanceof window.Element) {
+    el.appendChild(textOrPropsOrChild);
+  } else if (typeof textOrPropsOrChild === `string`) {
+    appendText(el, textOrPropsOrChild);
+  } else if (typeof textOrPropsOrChild === `object`) {
+    Object.keys(textOrPropsOrChild).forEach((propName) => {
+      el[propName] = textOrPropsOrChild[propName];
+    });
+  }
+
+  if (otherChildren) appendArray(el, otherChildren);
+
+  return el;
+}
+
+export const a = (...args) => makeElement(`a`, ...args);
+export const button = (...args) => makeElement(`button`, ...args);
+export const div = (...args) => makeElement(`div`, ...args);
+export const input = (...args) => makeElement(`input`, ...args);
+export const p = (...args) => makeElement(`p`, ...args);
