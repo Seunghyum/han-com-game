86716de6330a2dc3fe8e2fe36a9e63642e237985
seunghyum <antmgu@gmail.com>
2021-02-08 11:25:38 +0900

    feat: add webpack config and package.json - close #1;
    add webpack-** packages
    add wepback plugin packages
    add babel-loader and polyfill

---
+++ build/webpack.config.prod.js
@@ -0,0 +1,15 @@
+const { merge } = require("webpack-merge");
+const base = require("./webpack.config.base.js");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = merge(base, {
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
+});
