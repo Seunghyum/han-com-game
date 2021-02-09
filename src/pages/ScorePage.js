d722a77bb72bb53069827350394badebe90226b5
seunghyum <antmgu@gmail.com>
2021-02-09 12:45:49 +0900

    close #12 - feat: add ScorePage
    add banners and buton
    edit husky jest hook command in package.json

--- src/pages/ScorePage.js
+++ src/pages/ScorePage.js
@@ -1,20 +1,39 @@
 import { historyRouter, ROUTE_PATH } from '~src/router';
+import { div, p, span, button } from '~utils/vDom';
 class ScorePage {
-  render() {
-    const div = document.createElement('div');
-    const test = document.createElement('h1');
-    test.innerText = 'this is Score page';
-    const linkToScorePage = document.createElement('a');
-    linkToScorePage.innerText = 'to Game Page';
-    linkToScorePage.href = ROUTE_PATH.GamePage;
-    linkToScorePage.onclick = (event) => {
-      event.preventDefault();
-      historyRouter(ROUTE_PATH.GamePage);
-    };
+  constructor() {
+    this.$point = span();
+    this.$averageTime = span();
+  }
 
-    div.appendChild(test);
-    div.appendChild(linkToScorePage);
-    return div;
+  handleRestartGame() {
+    historyRouter(ROUTE_PATH.GamePage);
+  }
+
+  render() {
+    const { $point, $averageTime, handleRestartGame } = this;
+
+    return div({ className: 'container' }, [
+      p({ className: 'complete-banner' }, 'Mission Complete!'),
+      p({ className: 'point-banner' }, '당신의 점수는 ', $point, '점입니다.'),
+      p(
+        { className: 'average-time-banner' },
+        '단어당 평균 답변 시간은 ',
+        $averageTime,
+        '초입니다.'
+      ),
+      div(
+        { className: 'average-time-banner-wrapper' },
+        button(
+          {
+            type: 'button',
+            className: 'game-control__button',
+            onclick: handleRestartGame.bind(this),
+          },
+          '다시시작'
+        )
+      ),
+    ]);
   }
 }
 
