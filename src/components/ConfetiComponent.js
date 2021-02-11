454735d312119ed040641e422f51811f1eb6d165
seunghyum <antmgu@gmail.com>
2021-02-11 16:30:54 +0900

    chore: delete redundant comment

--- src/components/ConfetiComponent.js
+++ src/components/ConfetiComponent.js
@@ -3,11 +3,6 @@ import { div } from '~utils/vDom';
 function ConfettiBox() {
   return div(
     { className: 'confetti' },
-    // [
-    //   div({ className: 'confetti-piece' }),
-    //   div({ className: 'confetti-piece' }),
-    //   div({ className: 'confetti-piece' }),
-    // ]
     Array(18)
       .fill(null)
       .map(() => div({ className: 'confetti-piece' }))
